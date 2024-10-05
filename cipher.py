2fbb99c81f3e54d1a12444cb051484eb9d3759bd
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2024-10-05 14:58:16 +0200

    feat(cipher): create rot_47 cihper class

--- cipher.py
+++ cipher.py
@@ -33,3 +33,23 @@ class ROT13(Cipher):
         decrypted_text = decode(self.text, 'rot_13')
         self.status = False
         return decrypted_text
+
+
+class ROT47(Cipher):
+    def __init__(self):
+        super().__init__()
+        self.rot_type = 'rot_47'
+
+    def encrypt(self):
+        x = []
+        for i in range(len(self.text)):
+            j = ord(self.text[i])
+            if 33 <= j <= 126:
+                x.append(chr(33 + ((j + 14) % 94)))
+            else:
+                x.append(self.text[i])
+
+        return ''.join(x)
+
+    def decrypt(self):
+        return self.encrypt()
