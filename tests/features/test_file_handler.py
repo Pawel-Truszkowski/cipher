d2bb704fad4b2e2abc77f614a1fd62d720cc7b1f
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2024-11-03 20:34:55 +0100

    tests(file_handler): Add JSONFileHandler tests

--- tests/features/test_file_handler.py
+++ tests/features/test_file_handler.py
@@ -1,15 +1,15 @@
 import tempfile
 from ast import literal_eval
 import pytest
-from src.features.file_handler import TextFileHandler
+import json
+from src.features.file_handler import TextFileHandler, JSONFileHandler
 
 
 class TestTextFileHandler:
 
     @pytest.fixture
     def file_handler(self):
-        handler = TextFileHandler()
-        return handler
+        return TextFileHandler()
 
     def test_save_should_save_data_to_file(self, file_handler):
         data = {"original": "Hello", "encrypted": "Uryyb", "cipher": "rot13"}
@@ -61,4 +61,59 @@ class TestTextFileHandler:
 
 
 class TestJSONFileHandler:
-    pass
+    @pytest.fixture
+    def file_handler(self):
+        return JSONFileHandler()
+
+    def test_save_should_save_data_to_json_file(self, file_handler):
+        data = {"original": "Hello", "encrypted": "Uryyb", "cipher": "rot13"}
+
+        with tempfile.NamedTemporaryFile(mode="w+", delete=True) as temp_file:
+            file_handler.FILE_NAME = temp_file.name
+            file_handler.save(data)
+            temp_file.seek(0)
+            saved_content = json.load(temp_file)
+            assert saved_content == [data]
+
+    def test_read_all_should_return_data_when_file_is_not_empty(self, file_handler):
+        data1 = {"original": "Hello", "encrypted": "Uryyb", "cipher": "rot13"}
+        data2 = {"original": "World", "encrypted": "Jbeyq", "cipher": "rot13"}
+
+        with tempfile.NamedTemporaryFile(mode="w+", delete=True) as temp_file:
+            file_handler.FILE_NAME = temp_file.name
+            file_handler.save(data1)
+            file_handler.save(data2)
+
+            read_content = file_handler.read_all()
+            assert read_content == [data1, data2]
+
+    def test_read_all_should_return_empty_list_when_file_is_empty(self, file_handler):
+        with tempfile.NamedTemporaryFile(mode="w+", delete=True) as temp_file:
+            file_handler.FILE_NAME = temp_file.name
+            read_content = file_handler.read_all()
+            assert read_content == []
+
+    def test_read_should_print_all_data_when_file_is_not_empty(
+        self, file_handler, mocker
+    ):
+        data1 = {"original": "Hello", "encrypted": "Uryyb", "cipher": "rot13"}
+        data2 = {"original": "World", "encrypted": "Jbeyq", "cipher": "rot13"}
+
+        with tempfile.NamedTemporaryFile(mode="w+", delete=True) as temp_file:
+            file_handler.FILE_NAME = temp_file.name
+            file_handler.save(data1)
+            file_handler.save(data2)
+
+            mock_print = mocker.patch("builtins.print")
+            file_handler.read()
+
+            mock_print.assert_any_call("Messages from file:")
+            mock_print.assert_any_call(data1)
+            mock_print.assert_any_call(data2)
+
+    def test_read_should_print_message_when_file_is_empty(self, file_handler, mocker):
+        with tempfile.NamedTemporaryFile(mode="w+", delete=True) as temp_file:
+            file_handler.FILE_NAME = temp_file.name
+            mock_print = mocker.patch("builtins.print")
+            file_handler.read()
+            mock_print.assert_any_call("No messages to display.")
